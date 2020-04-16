d16a1003aa1d86e601066a02eb8492120dc69bc7
Christos Lytras <christos.lytras@gmail.com>
2020-04-16 12:30:14 +0300

    refactor main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import App from './App.svelte';
 // import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/scss/bootstrap.scss';
 import './App.scss';
-
 
 const app = new App({
 	target: document.body,
